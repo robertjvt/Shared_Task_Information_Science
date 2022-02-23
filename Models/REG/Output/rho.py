96c1cebd549ddcf1e5ea318252ef34687ac68d87
Jeunard19 <42543767+Jeunard19@users.noreply.github.com>
2022-02-23 14:48:27 +0100

    Add files via upload

---
+++ Models/REG/Output/rho.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import scipy.stats
+
+
+def main():
+
+    df = pd.read_csv('BERT_100_1E-05_10_3_8_MSE_ADAM_REG.csv')
+    x = df['Test']
+    y = df['Predict']
+    rho = scipy.stats.spearmanr(x, y)[0]
+    print(rho)
+
+
+if __name__ == "__main__":
+    main()
